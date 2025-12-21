6c7e5ac97e3fd998bff17e2ee8d188e6e5dc8433
Akshay <mandaakshay20@gmail.com>
2025-12-21 16:05:44 +0530

    Wrote code according to the description

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,30 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchLorem } from "../index";
+import "../styles/App.css";
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+  const dispatch = useDispatch();
+  const { loading, title, body, error } = useSelector((state) => state);
 
-export default App
+  useEffect(() => {
+    dispatch(fetchLorem());
+  }, [dispatch]);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
+
+  return (
+    <div className="container">
+      <h1>{title}</h1>
+      <p>{body}</p>
+    </div>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,82 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 import App from "./components/App";
 
+/* ---------------- REDUX SETUP ---------------- */
 
+// Initial State
+const initialState = {
+  loading: false,
+  title: "",
+  body: "",
+  error: ""
+};
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// Action Types
+const FETCH_REQUEST = "FETCH_REQUEST";
+const FETCH_SUCCESS = "FETCH_SUCCESS";
+const FETCH_FAILURE = "FETCH_FAILURE";
+
+// Reducer
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_REQUEST:
+      return { ...state, loading: true };
+
+    case FETCH_SUCCESS:
+      return {
+        loading: false,
+        title: action.payload.title,
+        body: action.payload.body,
+        error: ""
+      };
+
+    case FETCH_FAILURE:
+      return {
+        loading: false,
+        title: "",
+        body: "",
+        error: action.payload
+      };
+
+    default:
+      return state;
+  }
+};
+
+// Async Action (Thunk)
+export const fetchLorem = () => {
+  return async (dispatch) => {
+    dispatch({ type: FETCH_REQUEST });
+
+    try {
+      const res = await fetch("https://api.lorem.com/ipsum");
+      const data = await res.json();
+
+      dispatch({
+        type: FETCH_SUCCESS,
+        payload: data
+      });
+    } catch (err) {
+      dispatch({
+        type: FETCH_FAILURE,
+        payload: err.message
+      });
+    }
+  };
+};
+
+// Store
+const store = createStore(reducer, applyMiddleware(thunk));
+
+/* ---------------- RENDER ---------------- */
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
