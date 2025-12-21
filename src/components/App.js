eeaa37e0af9124d96ac96cdc1eac62f804c4e011
Akshay <mandaakshay20@gmail.com>
2025-12-21 16:20:05 +0530

    Corrected

--- src/components/App.js
+++ src/components/App.js
@@ -2,21 +2,16 @@ import React, { useEffect, useReducer } from "react";
 import "../styles/App.css";
 
 const initialState = {
-  loading: false,
-  title: "",
-  body: ""
+  loading: true,
+  posts: []
 };
 
 function reducer(state, action) {
   switch (action.type) {
-    case "FETCH_START":
-      return { ...state, loading: true };
-
     case "FETCH_SUCCESS":
       return {
         loading: false,
-        title: action.payload.title,
-        body: action.payload.body
+        posts: action.payload
       };
 
     default:
@@ -28,39 +23,46 @@ const App = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   useEffect(() => {
-    dispatch({ type: "FETCH_START" });
-
     fetch("https://api.lorem.com/ipsum")
       .then((res) => res.json())
       .then((data) => {
         dispatch({
           type: "FETCH_SUCCESS",
-          payload: data
+          payload: [
+            {
+              title: data.title,
+              body: data.body
+            }
+          ]
         });
       })
       .catch(() => {
         dispatch({
           type: "FETCH_SUCCESS",
-          payload: {
-            title: "Lorem Ipsum",
-            body: "Lorem ipsum dolor sit amet"
-          }
+          payload: [
+            {
+              title: "Lorem Ipsum",
+              body: "Lorem ipsum dolor sit amet"
+            }
+          ]
         });
       });
   }, []);
 
   return (
     <div className="container">
-      <h2>Lorem Ipsum</h2>
+      <h2>A short Naration of Lorem Ipsum</h2>
 
-      {state.loading ? (
-        <p>Loading...</p>
-      ) : (
-        <>
-          <h1>{state.title}</h1>
-          <p>{state.body}</p>
-        </>
-      )}
+      {state.loading && <h4>Loading...</h4>}
+
+      <ul>
+        {state.posts.map((post, index) => (
+          <li key={index}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
