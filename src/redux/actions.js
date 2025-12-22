75f41871b9f94f768e6e2a03380d0fadcdf337dd
Akshay <mandaakshay20@gmail.com>
2025-12-22 11:32:51 +0530

    Changes

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -8,7 +8,7 @@ export const fetchPosts = () => {
         payload: [
           {
             title: "Title :sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-            body: "Lorem ipsum dolor sit amet"
+            body: "Body :quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
           }
         ]
       });
