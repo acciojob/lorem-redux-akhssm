e49c09cc5bc353b5bc8fb41fb3a7a39f227b397e
Akshay <mandaakshay20@gmail.com>
2025-12-22 12:49:37 +0530

    Changed body

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -10,19 +10,7 @@ export const fetchPosts = () => {
             title:
               "Title :sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
             body:
-              "Body :Loading Body"
-          },
-          {
-            title:
-              "Title :sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-            body:
-              "Body :Loading Body"
-          },
-          {
-            title:
-              "Title :sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-            body:
-              "Body :Loading Body"
+              "Body :quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
           }
         ]
       });
